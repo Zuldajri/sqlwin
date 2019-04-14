60432a2de2b534b15c0694496954f567bc606c32
Zuldajri <w.matyas15@hotmail.fr>
2019-04-14 15:25:54 +0200

    Create sqllinux.sh

---
+++ sqllinux.sh
@@ -0,0 +1,28 @@
+fdisk -l /dev/sdc || break
+        fdisk /dev/sdc << EOF
+n
+p
+1
+
+
+p
+w
+EOF
+ 
+
+sudo mkfs -t ext4 /dev/sdc1
+
+sudo mkdir /datadrive
+
+sudo mount /dev/sdc1 /datadrive
+
+UUID=$(sudo -i blkid | grep /dev/sdc1 | awk 'NR==1 {print $2}' | sed 's/"//g')
+
+echo $UUID /datadrive              ext4    defaults,nofail 1 2  >> /etc/fstab
+
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+
+sudo firewall-cmd --zone=public --add-port=1433/tcp --permanent
+sudo firewall-cmd --reload
