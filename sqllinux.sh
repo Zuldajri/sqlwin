a9a5bc2753287828014cac5269b7c68e38b0ec4d
Zuldajri <w.matyas15@hotmail.fr>
2019-04-15 11:37:02 +0200

    Update sqllinux.sh

--- sqllinux.sh
+++ sqllinux.sh
@@ -1,3 +1,5 @@
+echo "SQL_PASSWORD=$SQL_PASSWORD"
+
 fdisk -l /dev/sdc || break
         fdisk /dev/sdc << EOF
 n
@@ -26,3 +28,9 @@ source ~/.bashrc
 
 sudo firewall-cmd --zone=public --add-port=1433/tcp --permanent
 sudo firewall-cmd --reload
+
+sudo systemctl stop mssql-server
+sudo /opt/mssql/bin/mssql-conf set-sa-password << EOF
+$SQL_PASSWORD
+$SQL_PASSWORD
+EOF
